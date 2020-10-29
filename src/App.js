4c37e3bd92539cc9051459c46bb0dcebbd6f3b38
myllyma <matti.myllymaki83@gmail.com>
2020-10-30 00:52:08 +0200

    2.1-2.3 complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import React, {useState, useEffect} from 'react';
 
 const workHoursSetUpList = [
-  {day: "maanantai", work_hours: 7},
-  {day: "tiistai", work_hours: 6},
-  {day: "keskiviikko", work_hours: 8},
-  {day: "torstai", work_hours: 5},
-  {day: "perjantai", work_hours: 8},
-  {day: "lauantai", work_hours: 2},
-  {day: "sunnuntai", work_hours: 3}
+  {name: "maanantai", work_hours: 5},
+  {name: "tiistai", work_hours: 7},
+  {name: "keskiviikko", work_hours: 3},
+  {name: "torstai", work_hours: 5},
+  {name: "perjantai", work_hours: 0},
+  {name: "lauantai", work_hours: 9},
+  {name: "sunnuntai", work_hours: 12}
 ];
 
 const WorkHours = () => {
@@ -18,9 +18,77 @@ const WorkHours = () => {
     setWeeklyWorkHours(workHoursSetUpList);
   }, []);
 
+  useEffect(() => {
+    console.log("weeklyWorkHours: ", weeklyWorkHours)
+  }, [weeklyWorkHours]);
+
+  // 2.2 a
+  const averageByFor = () => {
+    let average = 0;
+    for (let i = 0; i < weeklyWorkHours.length; ++i) {
+      average = average + weeklyWorkHours[i].work_hours;
+    }
+
+    average = average / weeklyWorkHours.length;
+    return(average);
+  }
+
+  // 2.2 b
+  const averageByReduce = () => {
+    let average = weeklyWorkHours.reduce((acc, cur) => {
+      return(acc + cur.work_hours);
+    }, 0);
+    
+    average = average / weeklyWorkHours.length;
+    return(average);
+  }
+
+  // 2.3 a
+  const minByFor = () => {
+    let minimumValue = Infinity;
+    for (let i = 0; i < weeklyWorkHours.length; ++i) {
+      minimumValue = Math.min(minimumValue, weeklyWorkHours[i].work_hours);
+    }
+    return(minimumValue);
+  }
+
+  // 2.3 b
+  const minByReduce = () => {
+    let minimumValue = weeklyWorkHours.reduce((min, cur) => Math.min(min, cur.work_hours), Infinity);
+    return(minimumValue);
+  }
+
+  // 2.3 a
+  const maxByFor = () => {
+    let maximumValue = -Infinity;
+    for (let i = 0; i < weeklyWorkHours.length; ++i) {
+      maximumValue = Math.max(maximumValue, weeklyWorkHours[i].work_hours);
+    }
+
+    return(maximumValue);
+  }
+
+  // 2.3 b
+  const maxByReduce = () => {
+    let maximumValue = weeklyWorkHours.reduce((max, cur) => Math.max(max, cur.work_hours), -Infinity);
+    return(maximumValue);
+  }
+
+  const handleWorkHourChange = (index) => (event) => {
+    const newWeeklyWorkHours = JSON.parse(JSON.stringify(weeklyWorkHours));
+    console.log("index: ", index, "event:", event.target.value);
+    newWeeklyWorkHours[index].work_hours = Number(event.target.value);
+    setWeeklyWorkHours(newWeeklyWorkHours);
+  }
+
+
   return(
     <div className="workHours">
-
+      <div className="weekdayListItem"><p className="label">Viikonpäivä</p><p>Käytetyt tunnit</p></div>
+      {weeklyWorkHours.map((day, index) => <div key={index} className="weekdayListItem"><p className="label">{day.name}</p><input value={weeklyWorkHours[index].work_hours} onChange={handleWorkHourChange(index)}></input></div>)}
+      <div className="weekdayListItem"><p className="label">keskiarvo</p><p>for: {averageByFor()} reduce: {averageByReduce()}</p></div>
+      <div className="weekdayListItem"><p className="label">min</p><p>for: {minByFor()} reduce: {minByReduce()}</p></div>
+      <div className="weekdayListItem"><p className="label">max</p><p>for: {maxByFor()} reduce: {maxByReduce()}</p></div>
     </div>
   );
 }
